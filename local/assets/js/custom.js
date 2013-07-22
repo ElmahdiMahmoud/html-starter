d6aaef59c5e1b71b52c2111377c64e472aa43f48
Mahmoud Elmahdi <elmahdi.mahmoud@gmail.com>
2013-07-22 10:19:18 +0300

    new
    new files

---
+++ local/assets/js/custom.js
@@ -0,0 +1,15 @@
+/*! 
+ *  Written for 	: ?
+ *  Author      	: @IkantamCorp - @ElmahdiMahmoud
+ *  Updated     	: 22.07.2013
+ *  Further changes : @ElmahdiMahmoud
+ */
+
+ (function($) { 
+ $(document).ready(function() { 
+ 	
+	//set class to IE10
+	if (document.body.style.msTouchAction != undefined) { document.documentElement.className += "ie10"; }
+	
+ }); //end doc ready
+})(jQuery); 
