2e5335af9ea456b908c16954199fb083ca1d734a
Fikra Masri <elmahdi.mahmoud@gmail.com>
2013-10-22 12:10:36 +0300

    Update custom.js

--- local/assets/js/custom.js
+++ local/assets/js/custom.js
@@ -1,15 +1,12 @@
 /*! 
  *  Written for 	: ?
  *  Author      	: @IkantamCorp - @ElmahdiMahmoud
- *  Updated     	: 22.07.2013
+ *  Updated     	: 21.10.13
  *  Further changes : @ElmahdiMahmoud
  */
 
  (function($) { 
  $(document).ready(function() { 
- 	
-	//set class to IE10
-	if (document.body.style.msTouchAction != undefined) { document.documentElement.className += "ie10"; }
 	
  }); //end doc ready
 })(jQuery); 
